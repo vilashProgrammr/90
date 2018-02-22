31af516293f88a9b50a699685782f646b5d3254f
dj+13@programmr.com <dj+13@programmr.com>
2018-02-22 08:22:11 +0000

    FINISH TEST

--- index.php
+++ index.php
@@ -5,6 +5,11 @@ include_once 'dbconfig.php';
 if(isset($_GET['delete_id']))
 {
 	//delete logic here
+	//dj+13
+	//38GmLsekq9ZEqzOu0Ho9zKtR
+	$sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
+	mysql_query($sql_query);
+	header("Location: $_SERVER[PHP_SELF]");
 	
 }
 // delete condition
